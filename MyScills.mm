f1d566d917140f92d11d108bbf81e77f7476fef2
Derlok <zipp192@yandex.ru>
2016-08-12 19:11:55 +0300

    Карта проекта

---
+++ MyScills.mm
@@ -0,0 +1,28 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1471017676950" ID="ID_60690032" MODIFIED="1471018159475" TEXT="MyScills">
+<node BACKGROUND_COLOR="#ffcccc" CREATED="1471017712842" ID="ID_1323475632" MODIFIED="1471017751970" POSITION="right" STYLE="bubble" TEXT="&#x417;&#x410;&#x414;&#x410;&#x427;&#x418;">
+<node CREATED="1471017795175" ID="ID_1415412193" MODIFIED="1471017820956" TEXT="&#x414;&#x43e;&#x431;&#x430;&#x432;&#x43b;&#x435;&#x43d;&#x438;&#x435;">
+<icon BUILTIN="messagebox_warning"/>
+</node>
+<node CREATED="1471017864758" ID="ID_1931292157" MODIFIED="1471017897270" TEXT="&#x412;&#x44b;&#x434;&#x435;&#x43b;&#x435;&#x43d;&#x438;&#x435;">
+<icon BUILTIN="messagebox_warning"/>
+<node CREATED="1471017832839" ID="ID_1733709070" MODIFIED="1471017846354" TEXT="&#x423;&#x434;&#x430;&#x43b;&#x435;&#x43d;&#x438;&#x435;">
+<icon BUILTIN="messagebox_warning"/>
+</node>
+<node CREATED="1471017898654" ID="ID_272553471" MODIFIED="1471017907074" TEXT="&#x420;&#x435;&#x434;&#x430;&#x43a;&#x442;&#x438;&#x440;&#x43e;&#x432;&#x430;&#x43d;&#x438;&#x435;">
+<icon BUILTIN="messagebox_warning"/>
+</node>
+</node>
+<node CREATED="1471017848302" ID="ID_573048679" MODIFIED="1471017890538" TEXT="&#x41f;&#x435;&#x440;&#x435;&#x43c;&#x435;&#x449;&#x435;&#x43d;&#x438;&#x435;">
+<icon BUILTIN="closed"/>
+</node>
+</node>
+<node BACKGROUND_COLOR="#ccff66" CREATED="1471018160364" ID="ID_1098943405" MODIFIED="1471018183198" POSITION="left" STYLE="bubble" TEXT="&#x410;&#x43d;&#x434;&#x440;&#x43e;&#x439;&#x434;">
+<icon BUILTIN="attach"/>
+<node CREATED="1471018186243" ID="ID_370525056" MODIFIED="1471018267575" TEXT="&#x422;&#x435;&#x441;&#x442;&#x438;&#x440;&#x43e;&#x432;&#x430;&#x43d;&#x438;&#x435; 1 &#x44d;&#x442;&#x430;&#x43f;&#x430;">
+<icon BUILTIN="full-1"/>
+</node>
+</node>
+</node>
+</map>
