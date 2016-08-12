4b439c004a0c8583b42edc632c68f8d614940405
Derlok <zipp192@yandex.ru>
2016-08-12 18:53:59 +0300

    Загружаю проект на github

---
+++ Assets/Plugins/SQLiter/Scripts/SQLiteManager.cs
@@ -0,0 +1,135 @@
+﻿using UnityEngine;
+using System.IO;
+using System.Text;
+using System.Data;
+using Mono.Data.SqliteClient;
+using System;
+using System.Collections.Generic;
+/**
+* Скрипт для работы с бд sqlite
+*
+db = new SQLiteManager();
+*
+* Query - обычный sql запрос, не возращающий результат
+db.Query("SELECT rowid, text FROM tasks WHERE rowid = 1");
+*
+* QueryResult - sql на вывод результата. Возращает коллекцию LIst<string[]>
+List<string[]> result = db.QueryResult("SELECT rowid, text FROM tasks");
+foreach (var item in result)
+{
+  Debug.Log(item[1]);
+}
+* 
+*/
+
+namespace SQLiteDB
+{
+    public class SQLiteManager
+    {
+        public static SQLiteManager instance = null;
+        public bool debugMode = true;
+
+        private const string DB_NAME = "Skills";
+        private static readonly string DB_LOCATION = "URI=file:"
+           + Application.dataPath + Path.DirectorySeparatorChar
+           + "Plugins" + Path.DirectorySeparatorChar
+           + "SQLiter" + Path.DirectorySeparatorChar
+           + "Databases" + Path.DirectorySeparatorChar
+           + DB_NAME + ".db";
+
+        /// <summary>
+        /// Обьекты бд
+        /// </summary>
+        private IDbConnection connection = null;
+        private IDbCommand command = null;
+        private IDataReader reader = null;
+
+        public SQLiteManager()
+        {
+            if (instance == null)
+                instance = this;
+        }
+
+        /// <summary>
+        /// Просто запрос к бд без результата
+        /// INSERT OR REPLACE INTO таблица () VALUES (); 
+        /// DELETE FROM таблица WHERE ....
+        /// UPDATE OR REPLACE таблица SET поле = значение WHERE....
+        /// </summary>
+		/// <returns>id затронутой строки</returns>
+        public int Query(string text)
+        {
+            this.Open();
+            command.CommandText = text;
+            command.ExecuteNonQuery();
+
+			command.CommandText = "SELECT last_insert_rowid() AS RowId";
+			int rowId = Convert.ToInt32(command.ExecuteScalar());
+
+            this.Close();
+			return rowId;
+        }
+
+        /// <summary>
+        /// Запрос к бд с результатом без *
+        /// </summary>
+        /// <returns>Коллекция строк</returns>
+        public List<string[]> QueryResult(string text)
+        {
+            this.Open();
+            List<string[]> data = new List<string[]>();
+            command.CommandText = text;
+            reader = command.ExecuteReader();
+
+            //Количество данных
+            int num = text.Split(new string[] { "," }, StringSplitOptions.None).Length;
+            while (reader.Read())
+            {
+                string[] arr = new string[num];
+                for (int i = 0; i < num; i++)
+                {
+                    arr[i] = reader.GetString(i);
+                    
+                }
+                data.Add(arr);
+            }
+            this.Close();
+            return data;
+        }
+
+        /// <summary>
+        /// Открыть соединение с бд
+        /// </summary>
+        private void Open()
+        {
+            connection = (IDbConnection)new SqliteConnection(DB_LOCATION);
+            connection.Open();//Открываем соединение
+            command = connection.CreateCommand();
+
+            //Настройки бд
+            command.CommandText = "PRAGMA journal_mode = WAL";
+            command.ExecuteNonQuery();
+            command.CommandText = "PRAGMA synchronous = OFF";
+            command.ExecuteNonQuery();
+        }
+
+        /// <summary>
+        /// Закрытие соединения с бд
+        /// </summary>
+        private void Close()
+        {
+            if (reader != null && !reader.IsClosed)
+                reader.Close();
+            reader = null;
+
+            if (command != null)
+                command.Dispose();
+            command = null;
+
+            if (connection != null && connection.State != ConnectionState.Closed)
+                connection.Close();
+            connection = null;
+        }
+
+    }
+}
---
+++ Assets/scripts/TaskAdd.cs
@@ -0,0 +1,84 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using SQLiteDB;
+using System;
+using System.Collections.Generic;
+
+/**
+ *  Класс для работы с задачами
+ *  
+ *  version 1.0
+ */
+public class  TaskAdd: MonoBehaviour {
+
+    public GameObject rename;
+    public GameObject remove;
+    public GameObject doing;
+	public GameObject list;
+    public InputField inputText;
+
+    //Размеры
+    private int height = 37;
+    private int currentPosition = 0;
+
+    private SQLiteManager db;
+
+    public void Start()
+    {
+        db = new SQLiteManager();
+        this.getDoings();
+    }
+
+    /// <summary>
+    /// Добавить задачу
+    /// </summary>
+	public void addDoing()
+	{
+        //записываем задачу в бд
+        string formatData = DateTime.Now.ToString("yyyy-dd-MM hh:mm:ss");
+        int result = db.Query("INSERT OR REPLACE INTO tasks (text, data) VALUES('" + inputText.text + "', '" + formatData + "')");
+
+		//Размещаем задачи
+		Instans(inputText.text, result);
+
+        inputText.text = "";
+    }
+
+    /// <summary>
+    /// Достаем с бд все сохраненые задачи
+    /// </summary>
+    private void getDoings()
+    {
+        List<string[]> result = db.QueryResult("SELECT rowid, text, data FROM tasks");
+        foreach (var item in result)
+        {
+			Instans(item[1], Convert.ToInt32(item[0]));
+        }
+    }
+
+    /// <summary>
+    /// Добавляет обьект на поле
+    /// </summary>
+	private void Instans(string text, int id)
+    {
+        //размещаем
+        GameObject obj = Instantiate(doing) as GameObject;
+        obj.transform.parent = list.transform;
+        obj.transform.localScale = Vector3.one;
+        obj.transform.localPosition = new Vector3(0, currentPosition, 0);
+        currentPosition -= height;
+        //задаем текст
+        obj.transform.FindChild("Label").GetComponent<Text>().text = text;
+		obj.GetComponent<RectTransform>().sizeDelta = new Vector2(0, height);
+		//Cобытие изменение toggle
+		Toggle toggle = obj.GetComponent<Toggle>();
+		toggle.onValueChanged.AddListener ((on) => { 
+			if(on)
+				Debug.Log(id); 
+		});
+
+        //увеличиваем лист с задачами
+        list.GetComponent<RectTransform>().sizeDelta = new Vector2(0, (currentPosition * -1) + 20);
+    }
+}
